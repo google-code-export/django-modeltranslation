9f44487caf177a6856451b894b745033543db7c6
eschler <eschler@eb01185e-fd33-11dd-bc25-5f29055a2a2d>
2012-10-12 10:03:56 +0000

    Tagged release django-modeltranslation-0.1.

--- modeltranslation/management/commands/update_translation_fields.py
+++ modeltranslation/management/commands/update_translation_fields.py
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-from django.db.models import F, Q
-from django.core.management.base import (BaseCommand, CommandError,
-                                         NoArgsCommand)
 
-from modeltranslation.settings import DEFAULT_LANGUAGE
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError, NoArgsCommand
+
 from modeltranslation.translator import translator
 from modeltranslation.utils import build_localized_fieldname
 
-
 class Command(NoArgsCommand):
-    help = ('Updates the default translation fields of all or the specified'
-            'translated application using the value of the original field.')
-
-    def handle(self, **options):
-        print "Using default language:", DEFAULT_LANGUAGE
-        for model, trans_opts in translator._registry.items():
-            print "Updating data of model '%s'" % model
-            for fieldname in trans_opts.fields:
-                def_lang_fieldname = build_localized_fieldname(
-                    fieldname, DEFAULT_LANGUAGE)
-                # We'll only update fields which do not have an existing value
-                model.objects.filter(
-                    Q(**{def_lang_fieldname: None}) |
-                    Q(**{def_lang_fieldname: ""})).update(
-                        **{def_lang_fieldname: F(fieldname)})
+    help = 'Updates the default translation fields of all or the specified'\
+           'translated application using the value of the original field.'
+    # args = '[app_name]'
+        
+    def handle(self, **options):        
+        default_lang = settings.LANGUAGES[0][0]        
+        print "Using default language:", default_lang        
+        
+        for model, trans_opts in translator._registry.items():            
+            print "Updating data of model '%s'" % model   
+            for obj in model.objects.all():                            
+                for fieldname in trans_opts.fields:
+                    def_lang_fieldname = build_localized_fieldname(fieldname, default_lang)
+                    # print "setting %s from %s to %s." % (def_lang_fieldname, fieldname, obj.__dict__[fieldname])
+                    if not getattr(obj, def_lang_fieldname):
+                        setattr(obj, def_lang_fieldname, obj.__dict__[fieldname])
+                obj.save()
